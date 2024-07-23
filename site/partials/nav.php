ce85d7d2fe3ef72fcd9bbdf5393fd49a40a53426
waimea-lscammell-300DTD <lscammell@waimea.school.nz>
2024-07-23 12:09:40 +1200

    update

--- site/partials/nav.php
+++ site/partials/nav.php
@@ -3,25 +3,10 @@
 require_once '_session.php';
 ?>
 <nav id="main-nav">
-
     <menu hx-boost="true">
 
         <li><a href="index.php">Home</a>
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-        <li><a href="about.php">About</a></li>
-=======
-        <li><a href="about.php">About</a>
->>>>>>> Stashed changes
 
-            <?php if ($loggedIn): ?>
-            <li><a href="logout-user.php">Logout</a>
-            <?php else: ?>
-            <li><a href="form-login.php">Login</a>
-            <li><a href="form-signup.php">Sign Up</a>
-            <?php endif ?>
-
-=======
         <li><a href="about.php">About</a>
 
         <?php if ($loggedIn): ?>
@@ -30,8 +15,6 @@ require_once '_session.php';
             <li><a href="form-login.php">Login</a>
             <li><a href="form-signup.php">Sign Up</a>
         <?php endif ?>
-            
->>>>>>> 45d31bbe4663ed2a6c259b9b266b0999a3999534
 
     </menu>
 
