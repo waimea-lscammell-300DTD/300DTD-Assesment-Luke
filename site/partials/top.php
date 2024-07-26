3b9e4b980bfcca6425898114d9a11c1b457fbe1c
waimea-lscammell-300DTD <lscammell@waimea.school.nz>
2024-07-26 12:47:15 +1200

    update

--- site/partials/top.php
+++ site/partials/top.php
@@ -16,11 +16,10 @@
         <title><?= SITE_NAME ?></title>
 
         <!-- Load favicon -->
-        <link rel="icon" href="images/favicon.svg">
+        <link rel="icon" href="images/stand.jpg">
 
         <!-- Load stylesheets -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.fluid.classless.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css">
+
         <link rel="stylesheet" href="css/styles.css">
 
         <!-- Load HTMX library -->
--- site/process-signup.php
+++ site/process-signup.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once '_session.php';
 require_once 'lib/debug.php';
 require_once 'lib/db.php';
 
@@ -11,20 +11,18 @@ $pass = $_POST['password'];
 
 // Hash the supplied password
 $hash = password_hash($pass, PASSWORD_DEFAULT);
-consoleLog($hash, 'Hashed Password');
+
 
 // Connect
 $db = connectToDB();
 // Add the user account
 $query = 'INSERT INTO users (forename, surname, username, hash) VALUES (?, ?, ?, ?)';
 $stmt = $db->prepare($query);
-$stmt->execute([$fore, $sur, $user, $hash]);
+$stmt->execute([$fore, $sur, $user, $hash])
 
 echo '<h2>Account created!</h2>';
 
-
-
-
+?>
 
 
 
